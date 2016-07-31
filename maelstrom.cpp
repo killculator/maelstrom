c062a0390ac8fc9135c55e06ba346fc4739c3001
killculator <killculator@protonmail.ch>
2016-07-30 20:47:43 -0500

    Finished basic I/O, with new and improved bugs!

--- maelstrom.cpp
+++ maelstrom.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <fstream>
 #include <sys/time.h>
 
 using namespace std;
-
-//checks argument validity and order to be passed to other functions
-void checkArgs();
 
 class File{
   private:
     int height;
     int width;
     string contents;
+    string rName;
+    string wName;
   
   public:
     /* Basic getters and setters */
@@ -28,6 +27,14 @@ class File{
       return contents;
     }
 
+    string getRName(){
+      return this->rName;
+    }
+
+    string getWName(){
+      return this->wName;
+    }
+
     void setHeight(int h){
       height = h;
     }
@@ -40,16 +47,28 @@ class File{
       contents = c;
     }
 
+    void setRName(string s){
+      rName = s;
+    }
+
+    void setWName(string s){
+      wName = s;
+    }
+
     /* I/O functions */
     //loads file into File object contents
-    void load();
+    void load(File *f);
 
     //writes File object contents to output file
-    void write();
+    void write(File *f);
 };
 
 //for funsies, checks run time
 void time(clock_t t1, clock_t t2);
+
+//checks argument validity and order to be passed to other functions
+void checkArgs(int argc, char *argv[], File *f);
+
 
 
 
@@ -60,9 +79,18 @@ int main(int argc, char *argv[]){
   clock_t t1 = clock();
 
   File file;
+  File *filep = &file;
 
-  file.load();
-  file.write();
+  //cout << strcmp(file->getName(), " ") << endl;
+
+  checkArgs(argc, argv, filep);
+  cout << "Reading " << file.getRName() << endl;
+  cout << "Writing " << file.getWName() << endl;
+
+  file.load(filep);
+  //cout << file->getContents() << endl;
+
+  file.write(filep);
 
   clock_t t2 = clock();
   time(t1, t2);
@@ -73,8 +101,28 @@ int main(int argc, char *argv[]){
 
 
 /* Function defintions */
-void checkArgs(){
+void checkArgs(int argc, char *argv[], File *f){
+  bool output = false;
 
+  if (argv[1])
+    f->setRName(argv[1]);
+
+  for (int i = 0; i < argc; i++){
+    cout << "at argv[" << i << "] argv = " << argv[i] << endl;
+    if (!strcmp(argv[i],"-o")){
+      f->setWName(argv[i + 1]);
+      output = true;
+    }
+
+    //detect if no '-o' parameter was provided
+    //to determine default file name,
+    //supposedly date + filename
+    if (!output){
+      string name = "glitch-" + f->getRName();
+      f->setWName(name);
+    }
+
+  }
 }
 
 void time(clock_t t1, clock_t t2){
@@ -84,10 +132,24 @@ void time(clock_t t1, clock_t t2){
   cout << time << endl;
 }
 
-void File::load(){
-  cout << "To be defined... : [" << endl;
+void File::load(File *f){
+  string contents;
+  ifstream infile;
+
+  infile.open(f->getRName());
+  if (infile.is_open())
+    while (infile.good())
+      getline(infile, contents);
+
+  f->setContents(contents);
+  infile.close();
 }
 
-void File::write(){
-  cout << "To be defined... : [" << endl;
+void File::write(File *f){
+  ofstream outfile;
+
+  outfile.open(f->getWName());
+  outfile << f->getContents() << endl;
+
+  outfile.close();
 }
